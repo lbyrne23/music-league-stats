f384909fda14779d02d2a3a06d523fe38b3a72cd
Lucy Byrne <22296967+lbyrne23@users.noreply.github.com>
2026-02-19 01:07:52 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/music-league-stats/",
   server: {
     host: "::",
     port: 8080,
